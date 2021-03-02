6f12497ede2c107962ff3627bd41f8e4f8e20ab2
NerlyTon <nerlyton@gmail.com>
2021-03-02 17:57:13 -0600

    Recursion notes

---
+++ recursion.js
@@ -0,0 +1,4 @@
+// a function that calls itself
+// call stack - when a function is invoked is placed on top. When function ends its removed from the top 
+// Invoke the same function with a different input until you reach teh base case
+// base case: where the recursion stops
