95552adc99304d7f6eeba933eec79c6c45c650b9
NerlyTon <nerlyton@gmail.com>
2021-03-07 14:13:20 -0600

    map() code

--- hashTables.js
+++ hashTables.js
@@ -20,67 +20,80 @@
 
 // ----------------------------------------------------------------------------
 
+// In JS we dont need to create our own HashTable. We can use Map() or Object.
+// Below will be the JS code version example
 
-class HashTable {
-    constructor(size=53) {
-        this.keyMap = new Array(size)
-    }
+const map = new Map()
 
-    _hash(key) {
-        let total = 0
-        let WEIRD_PRIME = 31
-        for(let i = 0; i < Math.min(key.length, 100); i++) {
-            let char = key[i]
-            let value = char.charCodeAt(0) - 96
-            total = (total * WEIRD_PRIME + value) % this.keyMap.length
-        }
-        return total
-    }
+map.set('key1', '[value1]') 
+map.get('key1')
+map.delete('key1')
+map.get('key1').push('value2')
+map.has('key1')
+map.size
+return [...map.values()].filter(m => m.length > 1)
 
-    set(key,value) {
-        let index = this._hash(key)
-        if(!this.keyMap[index]) {
-            this.keyMap[index] = []
-        }
-        this.keyMap[index].push([key, value])
-    }
+// Below will be the from scratch code version example
+// class HashTable {
+//     constructor(size=53) {
+//         this.keyMap = new Array(size)
+//     }
 
-    get(key) {
-        let index = this._hash(key)
-        if(this.keyMap[index]) {
-            for(let i = 0; i < this.keyMap[index].length; i++) {
-                if(this.keyMap[index][i][0] === key) {
-                    return this.keyMap[index][i][1]
-                }
-            }
-        }
-        return undefined
-    }
+//     _hash(key) {
+//         let total = 0
+//         let WEIRD_PRIME = 31
+//         for(let i = 0; i < Math.min(key.length, 100); i++) {
+//             let char = key[i]
+//             let value = char.charCodeAt(0) - 96
+//             total = (total * WEIRD_PRIME + value) % this.keyMap.length
+//         }
+//         return total
+//     }
 
-    values() {
-        let valuesArray = []
-        for(let i = 0; i < this.keyMap.length; i++) {
-            if(this.keyMap[i]){
-                for(let j = 0; j < this.keyMap[i].length; j++) {
-                    // handles duplicates
-                    if(!valuesArray.includes(this.keyMap[i][j][1]))
-                    valuesArray.push(this.keyMap[i][j][1])
-                }
-            }
-        }
-        return valuesArray
-    }
+//     set(key,value) {
+//         let index = this._hash(key)
+//         if(!this.keyMap[index]) {
+//             this.keyMap[index] = []
+//         }
+//         this.keyMap[index].push([key, value])
+//     }
 
-    keys() {
-        let keysArray = []
-        for(let i = 0; i < this.keyMap.length; i++) {
-            if(this.keyMap[i]){
-                for(let j = 0; j < this.keyMap[i].length; j++) {
-                    if(!keysArray.includes(this.keyMap[i][j][0]))
-                    keysArray.push(this.keyMap[i][j][0])
-                }
-            }
-        }
-        return keysArray
-    }
-}
+//     get(key) {
+//         let index = this._hash(key)
+//         if(this.keyMap[index]) {
+//             for(let i = 0; i < this.keyMap[index].length; i++) {
+//                 if(this.keyMap[index][i][0] === key) {
+//                     return this.keyMap[index][i][1]
+//                 }
+//             }
+//         }
+//         return undefined
+//     }
+
+//     values() {
+//         let valuesArray = []
+//         for(let i = 0; i < this.keyMap.length; i++) {
+//             if(this.keyMap[i]){
+//                 for(let j = 0; j < this.keyMap[i].length; j++) {
+//                     // handles duplicates
+//                     if(!valuesArray.includes(this.keyMap[i][j][1]))
+//                     valuesArray.push(this.keyMap[i][j][1])
+//                 }
+//             }
+//         }
+//         return valuesArray
+//     }
+
+//     keys() {
+//         let keysArray = []
+//         for(let i = 0; i < this.keyMap.length; i++) {
+//             if(this.keyMap[i]){
+//                 for(let j = 0; j < this.keyMap[i].length; j++) {
+//                     if(!keysArray.includes(this.keyMap[i][j][0]))
+//                     keysArray.push(this.keyMap[i][j][0])
+//                 }
+//             }
+//         }
+//         return keysArray
+//     }
+// }
