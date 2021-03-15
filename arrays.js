c3072c26024965a4b4c2497e271329e7c9288b5a
NerlyTon <nerlyton@gmail.com>
2021-03-14 21:55:17 -0500

    First solution

--- arrays.js
+++ arrays.js
@@ -2,3 +2,20 @@
 // Problem: Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. If
 // any two numbers in the input array sum up to the target sum, the function should return them in an array.
 
+function twoNumberSum(array, targetSum) {
+    // Write your code here.
+      for(let i = 0; i < array.length; i++) {
+          const numberOne = array[i]
+          for(let j = i + 1; j < array.length; j++) {
+              const numberTwo = array[j]
+              if(numberOne + numberTwo === targetSum) {
+                  return [numberOne, numberTwo]
+              }
+          }
+      }
+          return []
+  }
+
+//   nested array means O(n^2) and for space is O(1)
+
+  
