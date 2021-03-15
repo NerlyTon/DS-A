0304a06b9e10d40b6925addcc75884b6b2344a7d
NerlyTon <nerlyton@gmail.com>
2021-03-14 21:53:37 -0500

    coding problem

---
+++ arrays.js
@@ -0,0 +1,4 @@
+// Two Number Sum
+// Problem: Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. If
+// any two numbers in the input array sum up to the target sum, the function should return them in an array.
+
