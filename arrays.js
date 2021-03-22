64136ac46d1151974535451e16acf4bc7d61ce72
NerlyTon <nerlyton@gmail.com>
2021-03-21 19:13:51 -0500

    longest consecutive sequence

--- arrays.js
+++ arrays.js
@@ -50,3 +50,28 @@ function twoNumberSum(array, targetSum) {
     }
     return []
   }
+
+//   Longest consecutive substring leetcode# 128 Level Hard.
+
+longestConsecutive = (nums) => {
+    let set = new Set()
+
+    for(let num of nums) {
+        set.add(num)
+    }
+
+    let longestStreak = 0
+
+    for(let num of set) {
+        if (!set.has(num - 1)){
+            let currentNum = num
+            let currentStreak = 1
+
+            while (set.has(currentNum + 1)) {
+                currentStreak += 1
+                currentNum += 1
+            }
+            longestStreak = Math.max(longestStreak, currentStreak)
+        }
+    }
+}
