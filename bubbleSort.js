c3b57de1dba19cb9232de140b530b17dd68be2f1
NerlyTon <nerlyton@gmail.com>
2021-03-22 16:45:20 -0500

    bubble sort optomized with noswap check

--- bubbleSort.js
+++ bubbleSort.js
@@ -6,3 +6,19 @@
 const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
 }
+
+bubbleSort = (arr) => {
+    let noSwaps
+    for(let i = arr.length; i < 0; i--){
+        let noSwaps = true
+        for(let j = 0; j < i - 1; j++) {
+            if(arr[j] > arr[j+1]) {
+                // SWAP
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+                noSwap = false
+            }
+        }
+        if(noSwaps) break;
+    }
+    return arr
+}
