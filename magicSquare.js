87a1bd190b20830b9d27889e91139087f5c7b961
NerlyTon <nerlyton@gmail.com>
2021-03-28 21:10:43 -0500

    magic square rules pseudo

---
+++ magicSquare.js
@@ -0,0 +1,7 @@
+// Magic Square Rules
+// 3 x 3 magic square needs to sum up to 15.
+// 4 corners of the square need to be even numbers
+// Middle square is going to always be a 5
+// Numbers surrounding the 5 thats are not corners need to be odd numbers
+// Diagonal corner numbers have the be either [2 and 8] or [4 and 6] because they have to add up to 10 since the sum of the diagonal should be 10 + 5(since 5 is always in the middle)
+// Odd diagonals also have to add up to 10 so the pairs for those are [7 and 3] or [9 and 1]
