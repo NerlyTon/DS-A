7be7c4d4cfb1a96567c6a93f78891ec57ab26802
Nerly Ton <nerlyton@gmail.com>
2021-05-13 18:17:07 -0500

    merging overlapping intervals

--- arrays.js
+++ arrays.js
@@ -171,3 +171,25 @@ function arrayOfProducts(array) {
       
       return newArray
   }
+
+//   merge overlapping intervals 
+function mergeOverlappingIntervals(array) {
+const sortedArray = array.sort((a, b) => a[0] - b[0])
+	
+	const mergedIntervals = []
+	let currentInterval = sortedArray[0]
+	mergedIntervals.push(currentInterval)
+	
+	for(const nextInterval of sortedArray) {
+		const [_, currentIntervalEnd] = currentInterval
+		const [nextIntervalStart, nextIntervalEnd] = nextInterval
+		
+		if (currentIntervalEnd >= nextIntervalStart) currentInterval[1] = Math.max(currentIntervalEnd, nextIntervalEnd)
+		else {
+			currentInterval = nextInterval
+			mergedIntervals.push(currentInterval)
+		}
+	}
+	return mergedIntervals
+}
+
