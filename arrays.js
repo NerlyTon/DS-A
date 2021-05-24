0f0038bdb10fa21456fe7944c57107bf009d1f7d
Nerly Ton <nerlyton@gmail.com>
2021-05-23 20:00:13 -0500

    second solution for array of products

--- arrays.js
+++ arrays.js
@@ -172,6 +172,31 @@ function arrayOfProducts(array) {
       return newArray
   }
 
+//   array of products (second solution)
+function arrayOfProducts(array) {
+    const products = new Array(array.length).fill(1)
+      const leftProducts = new Array(array.length).fill(1)
+      const rightProducts = new Array(array.length).fill(1)
+      
+      let leftRunningProduct = 1
+      for (let i = 0; i < array.length; i++) {
+          leftProducts[i] = leftRunningProduct
+          leftRunningProduct *= array[i]
+      }
+      
+      let rightRunningProduct = 1
+      for (let i = array.length - 1; i > -1; i--) {
+          rightProducts[i] = rightRunningProduct
+          rightRunningProduct *= array[i]
+      }
+      
+      for (let i = 0; i < array.length; i++) {
+          products[i] = leftProducts[i] * rightProducts[i]
+      }
+  
+      return products
+  }
+
 //   merge overlapping intervals 
 function mergeOverlappingIntervals(array) {
 const sortedArray = array.sort((a, b) => a[0] - b[0])
