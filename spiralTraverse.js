77375edd5379b65238d91598a352522fb0048e7f
Nerly Ton <nerlyton@gmail.com>
2021-05-23 20:01:35 -0500

    starting code for spiral traverse

---
+++ spiralTraverse.js
@@ -0,0 +1,65 @@
+function spiralTraverse(array) {
+ 
+	let newArray = []
+	
+
+
+let startRow
+let startCol
+let endRow
+let endCol
+
+
+}
+
+
+// 	for(let i = 0; i < array.length; i++) {
+// 		newArray.push(array[0][i])
+// 	}
+// 		newArray.push(array[1][array.length-1])
+		
+// 		newArray.push(array[2][array.length-1])
+// 	for (let i = array[3].length -1; i >= 0; i--) {
+//    newArray.push(array[3][i])
+		
+// }
+// 	newArray.push(array[2][0])
+// 	newArray.push(array[1][0])
+// 		console.log(newArray)
+	
+// 
+
+// 		// newArray.push(array[i + 1][smallerArr.length-1])
+		
+// 		// console.log("second", newArray.push(array[array.length-1][0 - i]))
+		
+// 		console.log(newArray)
+// 	}
+	
+// 
+
+// get all elements in the first array[1]
+// get the last elemenet of all the arrays after it array[2][arra.length-1]
+// get all elements in the array backwards
+
+
+// for(let i = 0; i < array.length; i++) {
+// 	let singleArray = array[i]
+// // 	bool will switch sides of matrix and the sides of the arrays between first and last matrix
+	
+// // 	First for loop iterates through all arrays between the first and last arrays in the matrix
+// // 		If bool == true grab values at the end of each array that is iterated
+// // 	  If bool == false grab values at the start of each array that is iterated
+
+
+// // Second for loop will grab 
+// 	for(let j = 0; j < singleArray.length; j++){
+
+// 		// newArray.push(array[i][j])		
+
+// 		newArray.push(array[i+1][array[j].length-1])
+// 		console.log(newArray)
+		
+// 	}
+// }
+// }
